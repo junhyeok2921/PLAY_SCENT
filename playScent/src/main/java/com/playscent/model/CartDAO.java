f017af09c8f93b918ff4eb850352a29c6ef84a61
jinhee oh <riricos153@naver.com>
2024-03-28 09:52:01 +0900

    지니 - 변경 사항

--- playScent/src/main/java/com/playscent/model/CartDAO.java
+++ playScent/src/main/java/com/playscent/model/CartDAO.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 import com.playscent.db.SqlSessionManager;
-import com.smhrd.model.Member;
+
 
 public class CartDAO {
 	// 세션을 생성해 줄 수 있는 Factory 생성
